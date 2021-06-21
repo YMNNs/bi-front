3b22bf99ff846034275ebb87fd6e78012da46c58
杨梦博 <i@stager.top>
2021-06-21 08:38:40 +0800

    - 同步all_tables.js

--- src/api/mock/mock_data.js
+++ src/api/mock/mock_data.js
@@ -11,13 +11,15 @@ export const mock_data = {
     tables: [
         {
             id: 1,
-            name: "豆瓣电影",
+            name: "豆瓣电影数据",
             editable: false,
+            description: "按照豆瓣评分排序展示电影的基本信息",
         },
         {
             id: 2,
-            name: "58同城",
+            name: "58租房数据",
             editable: true,
+            description: "展示58同城租房网站的户型等基本信息",
         },
     ],
 };
