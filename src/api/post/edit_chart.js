770f37173261f884995c2b6aaaf4ffdcca5a44e8
杨梦博 <i@stager.top>
2021-06-22 20:46:55 +0800

    - 使用...展开对象

--- src/api/post/edit_chart.js
+++ src/api/post/edit_chart.js
@@ -1,10 +1,5 @@
 import axios from "@/request";
 
-// 拆壳
 export function edit_chart(chart) {
-    const data = {};
-    Object.keys(chart).forEach((key) => {
-        data[key] = chart[key];
-    });
-    return axios.post("chart/edit", data);
+    return axios.post("chart/edit", { ...chart });
 }
