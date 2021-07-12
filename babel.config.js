9148be3b557b7a51db476f7e5ece7454c2b05b03
杨梦博 <i@stager.top>
2021-07-12 17:09:59 +0800

    使用cypress

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     presets: ['@vue/cli-plugin-babel/preset'],
     plugins: [
+        '@babel/plugin-transform-modules-commonjs',
         [
             'import',
             {
